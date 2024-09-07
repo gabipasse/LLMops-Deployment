86cac0b4aed186cde0769e7f3b7eea22eef7199b
rafael <rafael22070063@aluno.cesupa.br>
2024-09-07 02:07:06 -0300

    implementing main on auth

--- model_versioning/auth.py
+++ model_versioning/auth.py
@@ -3,26 +3,34 @@ from mlflow.server.auth.client import AuthServiceClient
 import os
 from dotenv import load_dotenv, set_key
 
-load_dotenv()
-tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
-username = os.getenv("MLFLOW_TRACKING_USERNAME")
-old_password = os.getenv("MLFLOW_TRACKING_PASSWORD")
-new_password = os.getenv("MLFLOW_TRACKING_NEW_PASSWORD")
 
-auth_client: AuthServiceClient = get_app_client("basic-auth", tracking_uri=tracking_uri)
+def main():
+    load_dotenv()
+    tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
+    username = os.getenv("MLFLOW_TRACKING_USERNAME")
+    old_password = os.getenv("MLFLOW_TRACKING_PASSWORD")
+    new_password = os.getenv("MLFLOW_TRACKING_NEW_PASSWORD")
 
-print(
-    f"New Passowrd: {new_password}\nUsername: {username}\nOld Password: {old_password}"
-)
+    auth_client: AuthServiceClient = get_app_client(
+        "basic-auth", tracking_uri=tracking_uri
+    )
 
-try:
-    auth_client.update_user_password(username=username, password=new_password)
-    print("Password updated successfully")
+    print(
+        f"New Passowrd: {new_password}\nUsername: {username}\nOld Password: {old_password}"
+    )
 
-except Exception as e:
-    print(f"Erro ao atualizar a senha: {e}")
-    exit(1)
+    try:
+        auth_client.update_user_password(username=username, password=new_password)
+        print("Password updated successfully")
 
-os.environ["MLFLOW_TRACKING_PASSWORD"] = new_password
-set_key(".env", "MLFLOW_TRACKING_PASSWORD", new_password)
-print("New password stored at .env")
+    except Exception as e:
+        print(f"Erro ao atualizar a senha: {e}")
+        exit(1)
+
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = new_password
+    set_key(".env", "MLFLOW_TRACKING_PASSWORD", new_password)
+    print("New password stored at .env")
+
+
+if __name__ == "__main__":
+    main()
